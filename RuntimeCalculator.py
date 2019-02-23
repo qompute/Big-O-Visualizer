16b4b5a04cced1fd74d3b66c69558079fab91ed5
Qinhong Chen <qinhong.chen@berkeley.edu>
2019-02-23 01:06:55 -0800

    added quicksort

--- RuntimeCalculator.py
+++ RuntimeCalculator.py
@@ -18,13 +18,19 @@ def test_algorithm(function, input_generator, size_range):
 x = range(1, 500, 2)
 
 y = test_algorithm(bubble_sort, random_list, x)
-plt.scatter(x, y, c='b', label='Bubble sort')
+plt.scatter(x, y, c='blue', label='Bubble sort')
 
 y = test_algorithm(selection_sort, random_list, x)
-plt.scatter(x, y, c='r', label='Selection sort')
+plt.scatter(x, y, c='red', label='Selection sort')
 
 y = test_algorithm(insertion_sort, random_list, x)
-plt.scatter(x, y, c='y', label='Insertion sort')
+plt.scatter(x, y, c='yellow', label='Insertion sort')
+
+y = test_algorithm(merge_sort, random_list, x)
+plt.scatter(x, y, c='green', label='Merge sort')
+
+y = test_algorithm(quicksort, random_list, x)
+plt.scatter(x, y, c='purple', label='Quicksort')
 
 plt.suptitle('Analysis of Sorting Algorithms')
 plt.xlabel('Size of Array (N)')
--- sorting.py
+++ sorting.py
@@ -27,9 +27,48 @@ def insertion_sort(lst):
         lst[j] = val
 
 # =================== O(N log N) algorithms ===================
+def merge_sort(lst):
+    if len(lst) > 1:
+        mid = len(lst) // 2
+        left, right = lst[:mid], lst[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                lst[k] = left[i]
+                i += 1
+            else:
+                lst[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            lst[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            lst[k] = right[j]
+            j += 1
+            k += 1
 
+def quicksort(lst, begin=0, end=None):
+    def partition(lst, begin, end):
+        pivot = begin
+        for i in range(begin + 1, end + 1):
+            if lst[i] <= lst[begin]:
+                pivot += 1
+                lst[i], lst[pivot] = lst[pivot], lst[i]
+        lst[pivot], lst[begin] = lst[begin], lst[pivot]
+        return pivot
+    if end is None:
+        end = len(lst) - 1
+    if begin >= end:
+        return
+    pivot = partition(lst, begin, end)
+    quicksort(lst, begin, pivot - 1)
+    quicksort(lst, pivot + 1, end)
 
 # lst = random_list(100, 1000)
 # print(lst)
-# insertion_sort(lst)
+# quicksort(lst)
 # print(lst)
